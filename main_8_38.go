d00d2441b93be2e685bf6cddf33cd8d4b6fa54b0
345dvl <k.f.kntn@gmail.com>
2022-01-05 22:55:24 +0900

    add: 8-38

---
+++ main_8_38.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// n := 1
+	// switch n {
+	// case 1, 2:
+	// 	fmt.Println("1 or 2")
+	// case 3, 4:
+	// 	fmt.Println("3 or 4")
+	// default:
+	// 	fmt.Println("other")
+  // }
+
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("other")
+	}
+}
