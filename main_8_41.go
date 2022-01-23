febf13159cbc5464657f2f7af043cd56d659a83a
345dvl <k.f.kntn@gmail.com>
2022-01-23 18:05:42 +0900

    add: 8-41

---
+++ main_8_41.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func Test() {
+	defer fmt.Println("END")
+	fmt.Println("START")
+}
+
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+}
+
+func main() {
+	Test()
+
+	// defer func() {
+	// 	fmt.Println("1")
+	// 	fmt.Println("2")
+	// 	fmt.Println("3")
+	// }()
+
+	RunDefer()
+
+	// リソース開放処理
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	file.Write([]byte("Hello"))
+}
